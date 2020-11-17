16d46f701c6d276f71f869e3a504b0c278b73f5b
Chris Smith <chris@cs278.org>
2020-11-17 23:07:18 +0000

    Prevent class redeclaration errors

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -2,8 +2,10 @@
 
 namespace Cs278\ComposerAudit;
 
-if (\PHP_VERSION_ID >= 70100) {
-    require __DIR__.'/ComposerPlugin.real.php';
-} else {
-    require __DIR__.'/ComposerPlugin.fake.php';
+if (!class_exists(__NAMESPACE__.'\\ComposerPlugin', false)) {
+    if (\PHP_VERSION_ID >= 70100) {
+        require __DIR__.'/ComposerPlugin.real.php';
+    } else {
+        require __DIR__.'/ComposerPlugin.fake.php';
+    }
 }
